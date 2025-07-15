bccf123fb31770775345216a2cc6ac767ccd077d
K. Venkata Kartik <125541852+Karthik123467@users.noreply.github.com>
2025-07-15 10:03:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ model = tf.keras.models.load_model(model_path, compile=False)
 # Streamlit UI
 st.markdown("""
     <h1 style='text-align: center; color: #9c27b0;'>🌸 Flower Image Classifier</h1>
-    <p style='text-align: center;'>Upload one or more flower images and let the AI guess their type!</p>
+    # <p style='text-align: center;'>Upload one or more flower images and let the AI guess their type!</p>
     <hr style="border: 1px solid #e0e0e0;">
 """, unsafe_allow_html=True)
 st.markdown("""
